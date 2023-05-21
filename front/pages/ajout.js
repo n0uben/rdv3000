2d85b052ce248fc6369583df5514f963b24e6d2e
n0uben <benjamin.noureux@gmail.com>
2023-05-21 17:04:46 +0200

    ajoute ebauche de form ajout

--- front/pages/ajout.js
+++ front/pages/ajout.js
@@ -4,8 +4,38 @@ import Layout from "@/components/layout";
 export default function Ajout() {
     return (
         <Layout>
-            <h1 className="text-3xl font-bold underline">Bonjour cest la page 2 !</h1>
-            <p><Link href="/" className="text-cyan-600">Retour a laccueil </Link></p>
+            <div className="my-12">
+                <Link href="/" className="bg-blue-500 px-5 py-2 rounded-full text-white">Retour à l'accueil</Link>
+            </div>
+
+            <h1 className="text-4xl mb-6">Ajouter un rendez-vous</h1>
+            <div className="grid grid-cols-1">
+                <label className="block">Titre</label>
+                <input className="mt-3 block w-full" type="text" placeholder="le titre de votre rdv"/>
+
+                {/*refactorer pour avoir un champ date et deux champs heure ??? */}
+                <label className="block mt-6">Date et heure de début</label>
+                <input className="mt-3 block w-full" type="datetime-local"/>
+
+                <label className="block mt-6">Date et heure de fin</label>
+                <input className="mt-3 block w-full" type="datetime-local"/>
+
+                <label className="block mt-6">Employé</label>
+                <select className="mt-3 block w-full">
+                    <option>Sélectionner un employé</option>
+                    <option>employee 1</option>
+                </select>
+
+                <label className="block mt-6">Client</label>
+                <select className="mt-3 block w-full">
+                    <option>Sélectionner un client</option>
+                    <option>client 1</option>
+                </select>
+
+            </div>
+            <div className="mt-9">
+                <button className="bg-green-600 px-5 py-2 rounded-full text-white">Ajouter</button>
+            </div>
         </Layout>
     );
 }
--- front/pages/index.js
+++ front/pages/index.js
@@ -13,44 +13,53 @@ export async function getStaticProps() {
 }
 
 export default function Home({allRendezVous}) {
-    const options = {
+    const optionsDate = {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
+    };
+
+    const optionsHeure = {
         hour: '2-digit',
         minute: '2-digit',
-    };
+    }
 
     return (
         <Layout>
+            <div className="my-12">
+                <Link href="/ajout" className="bg-blue-500 px-5 py-2 rounded-full text-white">Ajouter un rendez-vous</Link>
+            </div>
+
+            <h1 className="text-4xl mb-6">Liste des rendez-vous</h1>
+
             <table className="table-auto">
                 <thead>
                 <tr>
+                    <th className="px-4 py-2">Titre</th>
                     <th className="px-4 py-2">Date</th>
+                    <th className="px-4 py-2">Début</th>
+                    <th className="px-4 py-2">Fin</th>
                     <th className="px-4 py-2">Employee</th>
                     <th className="px-4 py-2">Client</th>
                     <th className="px-4 py-2">Email client</th>
-                    <th className="px-4 py-2">Duree</th>
                 </tr>
                 </thead>
                 <tbody>
 
                 {allRendezVous.map(rendezvous => (
                     <tr key={rendezvous.id}>
-                        <td className="border px-4 py-2">{new Date(rendezvous.start).toLocaleString('fr-FR', options)}</td>
+                        <td className="border px-4 py-2">{rendezvous.title}</td>
+                        <td className="border px-4 py-2">{new Date(rendezvous.start).toLocaleString('fr-FR', optionsDate)}</td>
+                        <td className="border px-4 py-2">{new Date(rendezvous.start).toLocaleString('fr-FR', optionsHeure)}</td>
+                        <td className="border px-4 py-2">{new Date(rendezvous.end).toLocaleString('fr-FR', optionsHeure)}</td>
                         <td className="border px-4 py-2">{rendezvous.employee.firstName} {rendezvous.employee.lastName}</td>
                         <td className="border px-4 py-2">{rendezvous.client.firstName} {rendezvous.client.lastName}</td>
-                        <td className="border px-4 py-2">{rendezvous.client.email}</td>
                         <td className="border px-4 py-2">{rendezvous.client.email}</td>
                     </tr>
                 ))}
                 </tbody>
 
             </table>
-
-
-            <p><Link href="/ajout" className="text-cyan-600">Page 2</Link></p>
-
         </Layout>
     )
 }
